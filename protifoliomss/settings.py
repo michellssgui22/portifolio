fe5592392899ce6d0dfbc9106302e6f815615431
michellss22 <michellss.gui@gmail.com>
2023-01-04 09:47:54 -0300

    Editado o runtime

--- protifoliomss/settings.py
+++ protifoliomss/settings.py
@@ -154,5 +154,3 @@ LOGOUT_REDIRECT_URL = 'index'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
